dd255caf10bc8f06f76b9b690410b1783812e32b
Paul Butler <butlerpaul1@hotmail.com>
2022-09-05 17:07:21 +0100

    Adding http trigger to get and update user counts

---
+++ api/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+[assembly: FunctionsStartup(typeof(FunctionApp.Startup))]
+
+namespace FunctionApp
+{
+  /// <summary>
+  /// Implement the functions startup abstract class
+  /// </summary>
+  class Startup : FunctionsStartup
+  {
+    public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+    {
+      string cs = Environment.GetEnvironmentVariable("AzureConnectionString");
+      builder.ConfigurationBuilder.AddAzureAppConfiguration(cs);
+    }
+
+    /// <summary>
+    /// Overwrite the configure app configuration method with the azure app configuration provider
+    /// </summary>
+    /// <param name="builder"></param>
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+    }
+  }
+}
