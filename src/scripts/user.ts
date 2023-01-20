1720568af635f138d2576c3338e5af46ec823271
Paul Butler <butlerpaul1@hotmail.com>
2023-01-20 18:06:56 +0000

    Adding log message

--- src/scripts/user.ts
+++ src/scripts/user.ts
@@ -1,6 +1,7 @@
 ///Call the api to retrieve the current user count
 function getUserCount() {
     const url = 'api/visitorCounter'
+    console.log('Calling: ' + url )
     fetch(url)
         .then(data => {
             return data.json()
