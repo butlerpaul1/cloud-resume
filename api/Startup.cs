ad13df458e4a2b6e2a73804928fc42a308b91ad1
Paul Butler <butlerpaul1@hotmail.com>
2023-05-08 15:17:54 +0100

    Adding support for Azure Key Vault

--- api/Startup.cs
+++ api/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Azure.Identity;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 
@@ -6,23 +7,30 @@ using Microsoft.Extensions.Configuration;
 
 namespace FunctionApp
 {
-  /// <summary>
-  /// Implement the functions startup abstract class
-  /// </summary>
-  class Startup : FunctionsStartup
-  {
-    public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
-    {
-      string cs = Environment.GetEnvironmentVariable("AzureConnectionString");
-      builder.ConfigurationBuilder.AddAzureAppConfiguration(cs);
-    }
-
     /// <summary>
-    /// Overwrite the configure app configuration method with the azure app configuration provider
+    /// Implement the functions startup abstract class
     /// </summary>
-    /// <param name="builder"></param>
-    public override void Configure(IFunctionsHostBuilder builder)
+    class Startup : FunctionsStartup
     {
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            string AppConfigConnString = Environment.GetEnvironmentVariable("AzureConnectionString");
+            string keyVault = Environment.GetEnvironmentVariable("keyVaultName");
+            string keyVaultURI = string.Format("https://{0}.vault.azure.net/", keyVault);
+
+            builder.ConfigurationBuilder
+                          .AddAzureAppConfiguration(AppConfigConnString)
+                          .AddEnvironmentVariables()
+                          .AddAzureKeyVault(new Uri(keyVaultURI), new DefaultAzureCredential())
+                          .Build();
+        }
+
+        /// <summary>
+        /// Overwrite the configure app configuration method with the azure app configuration provider
+        /// </summary>
+        /// <param name="builder"></param>
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+        }
     }
-  }
 }
